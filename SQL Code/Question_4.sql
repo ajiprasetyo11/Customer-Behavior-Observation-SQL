dc5de77d6b2d386a35d65f294fdfff6186e6be3f
Aji Prasetyo <117008370+ajiprasetyo11@users.noreply.github.com>
2023-09-16 12:26:58 +0700

    Add files via upload

---
+++ SQL Code/Question_4.sql
@@ -0,0 +1,31 @@
+/* QUESTION 4 */
+-- Get the top 5 most profitable product and it's profit detail breakdown by month.
+
+
+WITH product_rank_permonth AS
+(
+  SELECT
+    *,
+    RANK() OVER(PARTITION BY month_year ORDER BY profit DESC) AS rank_permonth
+  FROM
+  (
+    SELECT
+      EXTRACT(YEAR FROM oi.created_at) AS year,
+      EXTRACT(MONTH FROM oi.created_at) AS month,
+      FORMAT_DATE('%B, %Y', oi.created_at) AS month_year,
+      oi.product_id,
+      p.name AS product_name,
+      ROUND(SUM(oi.sale_price),2) AS sales,
+      ROUND(SUM(p.cost),2) AS cost,
+      ROUND(SUM(oi.sale_price-p.cost),2) AS profit
+    FROM `bigquery-public-data.thelook_ecommerce.order_items` oi
+    LEFT JOIN `bigquery-public-data.thelook_ecommerce.products` p ON oi.product_id = p.id
+    WHERE
+      oi.status = 'Complete'
+    GROUP BY 1,2,3,4,5
+  )
+  ORDER BY 1,2
+)
+SELECT *
+FROM product_rank_permonth
+WHERE rank_permonth <= 5
